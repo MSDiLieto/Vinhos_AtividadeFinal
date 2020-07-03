b13fdd4599e769bbfeb06fa33250b44d114b5f5f
Marcelo Sousa Di Lieto <marcelo-souza94@hotmail.com>
2020-07-02 23:33:29 -0300

    Projeto criado

---
+++ contato/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Message(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        super(Message, self).save(*args, **kwargs)
---
+++ contato/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Message
+from .forms import MessageForm
+from django.shortcuts import render, redirect
+
+class MessageView(CreateView):
+    model = Message
+    fields = ('name', 'email', 'message')
+    template_name = 'contato/index.html'
+
+    def post(self, request, *args, **kwargs):
+        form = MessageForm(data=request.POST)
+
+        if form.is_valid():
+            new_message = form.save(commit=False)
+            new_message.save()
+            return redirect('contato:contato_index')
+        return self.render_to_response({'from', form})
